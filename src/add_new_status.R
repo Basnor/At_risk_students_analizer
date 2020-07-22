2bf20d8247c7b33282496290e696421ac7666678
Basnor <basnor@mail.ru>
2020-07-22 20:30:08 +0700

    change speed of wrong function

--- src/add_new_status.R
+++ src/add_new_status.R
@@ -1,21 +1,36 @@
 library(readxl)
 df <- read_excel("Students.xlsx")
-#View(df)
 
-# Не запускать!!!! Сначала подумай и переделай нормально. (каким-то чудом сработало)
-for (i in 1:length(df$Статус2)){
-  if (df$Статус2[i] == "Выпускник") {
-    
+# Добавляем статус выпускник всем, кто доучился до 8 семестра
+for (i in 1:nrow(df)){
+  if (df$Семестр[i] == 8){
+    df$Статус[i] <- "Выпускник"
+  }
+}
+
+# Сортировка по Студент и Семестр
+df <- df[order(df$Студент, df$Семестр),]
+
+# Не запускать!!!! (очень долгий)
+for (i in 1:nrow(df)){
+  print(i)
+  if (df$Статус[i] == "Выпускник" & df$Семестр[i] == 8) {
     id <- df$Студент[i]
     
-    for (j in 1:length(df$Студент)){
+    for (j in 1:i){
       if (df$Студент[j] == id) {
-        df$Статус2[j] <- "Выпускник"
+        df$Статус[j] <- "Выпускник"
       }
     }
   }
 }
 
-write.csv(df,"Students_09_03_01.csv", row.names = TRUE)
+# При проблемах с совместимостью кодировок выполнять так
+install.packages("writexl")
+library(writexl)
+write_xlsx(df,"Students_ISHITR_Classified.xlsx")
+
+# Если только на Linux выполняем
+#write.csv(df,"Students_09_03_01.csv", row.names = TRUE)
 
 
